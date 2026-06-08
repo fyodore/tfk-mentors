b162da9acda6d1cafb42a9594ada37975fe86e66
Ted Stepanoff <fyodore@gmail.com>
2026-06-07 20:23:24 -0500

    updating wsgi file

--- tfk_mentors/wsgi.py
+++ tfk_mentors/wsgi.py
@@ -11,6 +11,12 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tfk_mentors.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tfk_mentors.settings")
+
+# Apache/mod_wsgi on older Ubuntu cannot pass env vars into WSGIDaemonProcess.
+# When production.py exists on the server, use production settings automatically.
+_production_settings = os.path.join(os.path.dirname(__file__), "production.py")
+if os.path.isfile(_production_settings):
+    os.environ["DJANGO_SETTINGS_MODULE"] = "tfk_mentors.production"
 
 application = get_wsgi_application()
