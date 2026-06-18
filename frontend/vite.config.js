081e694a65284671533bce6ea224b247d9a9eb1f
Ted Stepanoff <fyodore@gmail.com>
2026-06-17 22:19:21 -0500

    Update front end config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 700,
+    chunkSizeWarningLimit: 900,
   },
   server: {
     host: true,
